f55ea6d5454c4e212d5a0b860883d67a5ae89fc3
Daniel D <ddunnemann@gmail.com>
2024-08-19 17:00:26 -0500

    Login Testing 003

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 // If a POST request is made to /api/users, a new user is created. The user id and logged in state is saved to the session within the request object.
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({ where: { user_name: req.body.user_name } });
 
     if (userData) {
         res.status(400).render('login', { message: 'Invalid username or password' });
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
     const newUser = await User.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = newUser.user_id;
       req.session.userLoggedIn = true;
       res.redirect('/dashboard');
     });
@@ -28,7 +28,7 @@ router.post('/', async (req, res) => {
 // If a POST request is made to /api/users/login, the function checks to see if the user information matches the information in the database and logs the user in. If correct, the user ID and logged-in state are saved to the session within the request object.
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { user_name: req.body.username } });
+    const userData = await User.findOne({ where: { user_name: req.body.user_name } });
 
     if (!userData) {
       res
@@ -50,7 +50,6 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.userLoggedIn = true;
       res.redirect('/dashboard');
-      res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
@@ -70,7 +69,7 @@ router.post('/dashboard', (req, res) => {
 
 // If a POST request is made to /api/users/logout, the function checks the logged_in state in the request.session object and destroys that session if logged_in is true.
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.userLoggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
