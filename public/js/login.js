d87741d016f43ebb71082a2cdddd04da9d0cb703
Daniel D <ddunnemann@gmail.com>
2024-08-19 17:35:44 -0500

    Login Testing 003

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,21 @@
 const loginHandling = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector('#username').value.trim();
+    const user_name = document.querySelector('#username').value.trim();
     const password = document.querySelector('#password').value.trim();
     
-    if (username && password) {
+    if (user_name && password) {
             let response;
             if (event.target.id ==='login-button') {
                 response = await fetch('/api/users/login', {
                     method: 'POST',
-                    body: JSON.stringify({ username, password }),
+                    body: JSON.stringify({ user_name, password }),
                     headers: { 'Content-Type': 'application/json' }
                 });
             } else if (event.target.id === 'reg-button') { 
                 response = await fetch('/api/users/', {
                 method: 'POST',
-                body: JSON.stringify({ username, password }),
+                body: JSON.stringify({ user_name, password }),
                 headers: { 'Content-Type': 'application/json' },
                 });
             }
