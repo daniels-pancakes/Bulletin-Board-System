88f6b40c8d84f0fb632754a0a9f255d133381436
Daniel D <ddunnemann@gmail.com>
2024-08-19 11:26:45 -0500

    Addtl troubleshooting to implement postgreSQL DB hosting on Render

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,17 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
+    ? new Sequelize(process.env.DB_URL, {
+    // Required adding below options to enable SSL
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        },
+    })
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -11,6 +21,12 @@ const sequelize = process.env.DB_URL
             host: process.env.DB_HOST || 'localhost',
             dialect: 'postgres',
             port: process.env.DB_PORT || 5432,
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false,
+                }
+            },
         }
     );
 
